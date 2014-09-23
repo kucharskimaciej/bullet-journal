5be751cf42dd39804ec2efb0bb1c11fb12884ccf
Maciej Kucharski <maciej.kucharski@naturaily.com>
2014-09-24 00:28:15 +0200

    add: database connection

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 // External libs
 var Hapi = require('hapi');
 var Path = require('path');
+var mongoose = require('mongoose');
 
 // Configs
 var config = require('./config.js');
 
-
+mongoose.connect(config.database.host + ':' + config.database.port + '/' + config.database.db, {
+    user: config.database.username,
+    pass: config.database.password
+});
 
 Hapi.Pack.compose(config.manifest, { relativeTo: Path.join(__dirname, 'modules') },function (err, pack) {
     pack.start(function () {
         console.log("Server started.");
     });
-
 });
