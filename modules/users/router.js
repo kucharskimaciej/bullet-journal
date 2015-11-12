ec44072b27373cca36f26856a3d46e38cac54c3b
Maciej Kucharski <maciek.zir@gmail.com>
2015-11-12 22:20:27 +0100

    [master] cleanup old code Previous implementation moved to `legacy` branch. This will merge the front-end repository too.

--- modules/users/router.js
+++
@@ -1,1 +0,0 @@
-var Ctrl = require('./controller')
--- server.js
+++ server.js
@@ -8,33 +8,6 @@ var mongoose = require('mongoose');
 var env = process.env.NODE_ENV || "development";
 var config = require('./config.js');
 
-// create server
-var server = new Hapi.Server();
-
-// connect to the DB
-mongoose.connect(config.database[env].host + ':' + config.database[env].port + '/' + config.database[env].db, {
-    user: config.database[env].username,
-    pass: config.database[env].password
-});
-
-// create connections (pack)
-config.manifest.servers.forEach(function (options) {
-    server.connection(options)
-});
-
-// register plugins
-Object.keys(config.manifest.plugins).forEach(function (name) {
-    var plugin;
-    plugin = require(Path.join(__dirname, 'modules', name));
-    plugin.options = config.manifest.plugins[name];
-
-    server.register(plugin, function (err) {
-        if(err) {
-            console.log(err);
-        }
-    });
-});
-
 // start server
 server.start(function () {
     console.log('Server running at:', server.info.uri);
