b6ca6d964c1c02314bc6a9a03aa8b539d041e7e1
Maciej Kucharski <maciek.zir@gmail.com>
2016-03-22 14:18:29 +0100

    [webpack] routing with basic pages

--- src/bullet/client/index.tsx
+++ src/bullet/client/index.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../../../typings/browser.d.ts" />
 /// <reference path='../../../lib/react-komposer.d.ts' />
 /// <reference path='../../../lib/flow-router.d.ts' />
-import {FlowRouter} from 'meteor/meteorhacks:flow-router';
+import {FlowRouter} from 'meteor/kadira:flow-router';
 import './routes';
 
 Meteor.startup(function() {
--- src/bullet/client/routes.tsx
+++ src/bullet/client/routes.tsx
@@ -1,16 +1,75 @@
-import {FlowRouter} from 'meteor/meteorhacks:flow-router';
+import {FlowRouter, TriggerHandler} from 'meteor/kadira:flow-router';
+import * as React from 'react';
 import {render} from 'react-dom';
 
 import {BulletLayout} from './components/layout/bullet';
 import {Bullet} from './components/bullet/component';
 
 const APPLICATION_ROOT_ID = 'app';
+
 function renderRoot(jsx) {
     return render(jsx, document.getElementById(APPLICATION_ROOT_ID));
 }
 
-FlowRouter.route('/', {
+const requireLogin:TriggerHandler = () => {
+    if (!(Meteor.loggingIn() || Meteor.userId())) {
+        let {route} = FlowRouter.current();
+
+        if (route.name !== 'login') {
+            Session.set('redirect', route.path);
+        }
+
+        FlowRouter.go('login');
+    }
+};
+
+const protectedRoutes = FlowRouter.group({
+    triggersEnter: [requireLogin]
+});
+
+const publicRoutes = FlowRouter.group({});
+
+publicRoutes.route('/login', {
+    name: 'login',
     action() {
-        renderRoot(<BulletLayout><Bullet /></BulletLayout>);
+        renderRoot(<h1>LOGIN</h1>);
+    }
+});
+
+protectedRoutes.route('/', {
+    name: 'root',
+    action() {
+        FlowRouter.go('home');
+    }
+});
+
+protectedRoutes.route('/home', {
+    name: 'home',
+    action() {
+        renderRoot(<h1>HOME</h1>);
+    }
+});
+
+protectedRoutes.route('/logout', {
+    name: 'logout',
+    action() {
+        Meteor.logout(() => {
+           FlowRouter.go('login');
+        });
+    }
+});
+
+FlowRouter.notFound = {
+    name: '404',
+    action() {
+        renderRoot(<h1>404</h1>);
+    }
+};
+
+
+Accounts.onLogin(() => {
+    const redirect = Session.get('redirect');
+    if (!!redirect && redirect !== '/login') {
+        FlowRouter.go(redirect);
     }
 });
