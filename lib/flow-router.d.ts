b5ae44cfdebbe66d1a6c72f4dbb3d33afe351c9e
Maciej Kucharski <maciek.zir@gmail.com>
2016-03-22 14:18:04 +0100

    [webpack] improved typings

--- lib/flow-router.d.ts
+++ lib/flow-router.d.ts
@@ -1,30 +1,79 @@
-declare module 'meteor/meteorhacks:flow-router' {
-    interface IRouteDefinition {
+declare module 'meteor/kadira:flow-router' {
+    interface State {
+        path: string;
+    }
+
+    interface Context {
+        canonicalPath: string;
+        hash: string;
+        init: boolean;
+        params: Object;
+        path: string;
+        pathname: string;
+        state: State;
+        title: string;
+    }
+
+    interface TriggerHandler {
+        (
+            context: RouteDefinition,
+            doRedirect: (url:string, params:Object, queryParams:Object) => void,
+            doStop: () => void
+        ): void;
+    }
+
+    interface GroupOptions {
+        triggerEnter?: TriggerHandler|TriggerHandler[];
+        triggerExit?: TriggerHandler|TriggerHandler[];
+        name?: string;
+        prefix?: string;
+        subscriptions?(params?:Object, queryParams?:Object): void;
+    }
+
+    interface RouteDefinition {
         pathDef: string;
         name: string;
         options?: Object;
     }
 
-    interface IPathDefinition {
+    interface RouteDefinition {
+        context?: Context;
         path: string;
         params?: Object;
         queryParams?: Object;
-        route: IRouteDefinition;
+        route: RouteDefinition;
+        oldRoute?: RouteDefinition;
+        group?: Group;
     }
 
-    interface IRouteOptions {
-        action?(params?: Object, queryParams?: Object): void;
+    interface RouteOptions {
+        name?: string;
+        action?(params: Object, queryParams: Object): void;
         subscriptions?(params?: Object, queryParams?: Object): void;
     }
 
+    interface Group {
+        route(path:string, routeOptions:RouteOptions): void;
+        group(options:GroupOptions): Group;
+    }
+
     export interface FlowRouter {
+        notFound(): void;
         initialize(): void;
         wait(): void;
-        route(path: string, routeOptions: IRouteOptions): void;
+        go(pathDef: string, params?: Object, queryParams?: Object): void;
+        path(pathDef: string, params?: Object, queryParams?: Object): string;
+        route(path:string, routeOptions:RouteOptions): void;
+        group(options:GroupOptions): Group;
 
         getParam(paramName: string): string;
-        current(): IPathDefinition;
+        setParams(newParams: Object): boolean;
+        setQueryParams(newParams: Object): boolean;
+        current(): RouteDefinition;
         watchPathChange(): void;
         reload(): void;
+        redirect(path: string): void;
+
+        subsReady(): boolean;
     }
 }
