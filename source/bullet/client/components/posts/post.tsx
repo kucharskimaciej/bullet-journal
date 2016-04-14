53cce4ed767d6723cf5752b053f596226a69022e
Maciej Kucharski <maciek.zir@gmail.com>
2016-04-14 13:59:45 +0200

    [split-source] added markdown compilation

--- source/bullet/client/components/posts/post.tsx
+++ source/bullet/client/components/posts/post.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import {Component, PropTypes} from 'react';
+import * as marked from 'marked';
 
 import {IPost} from '../../../collections/posts/posts';
 
 import {PostDate} from './post_date';
 
+
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: false,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: true
+});
 
 export class Post extends Component<IPost, {}> {
     static propTypes = {
@@ -15,6 +27,9 @@ export class Post extends Component<IPost, {}> {
 
     render() {
         const {body, title, created_at} = this.props;
+        const renderedBody = {
+            __html: marked(body)
+        };
 
         return (
             <article>
@@ -22,9 +37,7 @@ export class Post extends Component<IPost, {}> {
                     <PostDate createdAt={created_at} />
                     { title ? <h2>{title}</h2> : null }
                 </header>
-                <div>
-                    {body}
-                </div>
+                <div dangerouslySetInnerHTML={renderedBody}></div>
             </article>
         );
     }
