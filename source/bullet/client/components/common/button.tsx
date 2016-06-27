7136d0c87b3b500463c8a262c0c1173f18d0a83a
Maciej Kucharski <maciek.zir@gmail.com>
2016-06-27 11:37:19 +0200

    [master][feature] button presets

--- source/bullet/client/components/common/button.tsx
+++ source/bullet/client/components/common/button.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'; 
 import { Component } from 'react';
+import * as _ from 'underscore';
 
 interface IButtonProps {
     btnStyle?: string;
@@ -11,9 +12,14 @@ interface IButtonProps {
 const styles = require('./button.styl');
 
 
-export class Button extends Component<IButtonProps,{}> {
-    private static StyleClass = {
-        'primary': styles.buttonPrimary
+export default class Button extends Component<IButtonProps,{}> {
+    private static StyleClasses = {
+        'primary': styles.buttonPrimary,
+        'secodary': styles.buttonSecondary
+    };
+
+    private static SizeClasses = {
+        'small': styles.buttonSmall
     };
 
     static defaultProps = {
@@ -21,13 +27,29 @@ export class Button extends Component<IButtonProps,{}> {
         btnSize: '',
         btnType: 'button'
     };
+
+    get buttonClass () {
+        return `${Button.StyleClasses[this.props.btnStyle]} ${Button.SizeClasses[this.props.btnSize]}`;
+    }
     
     render() {
-        const {btnStyle, btnType, children} = this.props;
+        const {btnType, children} = this.props;
         return (
-            <button type={btnType} className={Button.StyleClass[btnStyle]} {...this.props}>
+            <button type={btnType} className={this.buttonClass} {...this.props}>
                 <span className={styles.inner}>{ children }</span>
             </button>
         );
     }
 }
+
+const createButtonPreset = (btnStyle?, btnSize?, BaseCls = Button) => {
+    return class extends BaseCls {
+        static defaultProps = Object.assign({}, BaseCls.defaultProps, {
+            btnStyle: 'primary'
+        });
+    };
+};
+
+
+export const PrimaryButton = createButtonPreset('primary');
+export const SecondaryButton = createButtonPreset('secondary');
