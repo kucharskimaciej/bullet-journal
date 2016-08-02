e94e57f348b2a0b22f2a5b07803473a44e07b533
Maciej Kucharski <maciek.zir@gmail.com>
2016-08-02 22:42:43 +0200

    [master][feature] separate collection for grid

--- imports/collections/gamification/collection.ts
+++ imports/collections/gamification/collection.ts
@@ -7,4 +7,13 @@ export interface IGamificationRecord {
     value: any;
 }
 
+export interface IGamificationGrid {
+    _id?: string;
+    key: string;
+    user_id: string;
+    date: number;
+    count: number;
+}
+
 export const GamificationRecords = new Mongo.Collection<IGamificationRecord>('gamification_records');
+export const GamificationGrid = new Mongo.Collection<IGamificationGrid>('gamification_grid');
