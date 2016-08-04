57a67ffb5fb0e8644327136c344dad5ddc985c62
Maciej Kucharski <maciek.zir@gmail.com>
2016-08-04 22:44:12 +0200

    [master][feature] always use UTC dates

--- imports/collections/helpers.ts
+++ imports/collections/helpers.ts
@@ -1,9 +1,10 @@
 import { compose } from 'underscore';
+import * as moment from 'moment';
 
 export const addCreatedAtTimestamp = (userId, document) => {
-    document.created_at = new Date();
+    document.created_at = moment().utc().toDate();
 };
 
 export const addUpdatedAtTimestamp = (userId, document) => {
-    document.updated_at = new Date();
+    document.updated_at = moment().utc().toDate();
 };
