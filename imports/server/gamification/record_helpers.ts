f8355664f9e6d69bb1a827648a1e5437fd7ed990
Maciej Kucharski <maciek.zir@gmail.com>
2016-08-05 22:55:39 +0200

    [master][fix] gamification issues when changing created_at to Date

--- imports/server/gamification/record_helpers.ts
+++ imports/server/gamification/record_helpers.ts
@@ -2,6 +2,8 @@ import {GamificationRecords} from '../../collections/gamification/collection';
 import * as moment from 'moment';
 import {ISubject} from "./subjects";
 
+export type timestampOrDate = number|Date;
+
 export const DAY_IN_MS = 24 * 60 * 60 * 1000;
 
 export interface ISubjectHandler<PayloadType> {
@@ -14,7 +16,7 @@ export const getRecord = (key, user_id) => GamificationRecords.findOne({
 
 export const getRecordOfType = key => user_id => getRecord(key, user_id);
 
-export const getDayStart = (date = Date.now()):number => {
+export const getDayStart = (date: timestampOrDate = Date.now()):number => {
     return moment(date).utc().startOf('day').valueOf();
 };
 
@@ -22,7 +24,7 @@ export const getLast30DaysStart = () => getDayStart(
     moment().subtract(30, 'days').valueOf()
 );
 
-export const getNextDayStart = (date = Date.now()):number => {
+export const getNextDayStart = (date: timestampOrDate = Date.now()):number => {
     return getDayStart(date) + DAY_IN_MS;
 };
 
