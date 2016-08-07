48c7d13913a63ee9b8d379eea532e033ddb0b080
Maciej Kucharski <maciek.zir@gmail.com>
2016-08-07 23:09:44 +0200

    [master][fix][refactor] typing problems with unnamed action classes

--- imports/client/actions/modals.ts
+++ imports/client/actions/modals.ts
@@ -1,8 +1,17 @@
 import {IPost} from "../../collections/posts/posts";
 import * as actions from './constants';
 import {modal} from '../constants';
+import {Action} from 'redux';
 
-const showModal = (type: string) => data => ({
+export interface ModalAction extends Action {
+    type: string;
+    payload?: {
+        type: string,
+        data: any;
+    };
+}
+
+const showModal = (type: string) => data => (<ModalAction>{
     type: actions.SHOW_MODAL,
     payload: {
         type, data
@@ -12,9 +21,9 @@ const showModal = (type: string) => data => ({
 export const showUpdateModal = (post: IPost) => 
     showModal(modal.UPDATE_POST)(post);
 
-export const showRemoveModal = (post: IPost) => 
+export const showRemoveModal = (post: IPost) =>
     showModal(modal.REMOVE_POST)(post);
 
-export const closeModal = () => ({
+export const closeModal = () => (<ModalAction>{
     type: actions.HIDE_MODAL
 });
--- imports/client/actions/notifications.ts
+++ imports/client/actions/notifications.ts
@@ -1,14 +1,23 @@
 import * as actions from './constants';
 import {notification} from '../constants';
 import {IPost} from "../../collections/posts/posts";
+import {Action} from 'redux';
 
-const showNotification = (type: string) => data => ({
+export interface NotificationAction extends Action {
+    type: string;
+    payload?: {
+        type: string,
+        data: any;
+    };
+}
+
+const showNotification = (type: string) => data => (<NotificationAction>{
     type: actions.SHOW_NOTIFICATION,
     payload: {
         type, data
     }
 });
 
-export const dismissNotification = () => ({ type: actions.DISMISS_NOTIFICATION });
+export const dismissNotification = () => (<NotificationAction>{ type: actions.DISMISS_NOTIFICATION });
 
 export const showPostRemoveNotification = showNotification(notification.ON_REMOVE_POST);
