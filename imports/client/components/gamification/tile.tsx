055620f8543db9672ded4d0b36416a59d4598f2d
Maciej Kucharski <maciek.zir@gmail.com>
2016-08-10 23:11:11 +0200

    [master][refactor] tile component

--- imports/client/components/gamification/tile.tsx
+++ imports/client/components/gamification/tile.tsx
@@ -10,33 +10,56 @@ export interface ITileProps {
     value: any;
 }
 
+export const tileContentTemplate = {
+    [KEYS.BUSIEST_DAY](value) {
+        return (
+            <section>
+                <span>{value.count}</span>
+                <span>{value.date}</span>
+            </section>
+        );
+    },
+    [KEYS.HOT_STREAK](value) {
+        let content;
+        const end = moment(value.end).utc();
+        if (moment().diff(end, 'days') > 1) {
+            content = 0;
+        } else {
+            content = moment(value.end).diff(moment(value.start), 'days');
+        }
+
+        return (
+            <section>
+                <span>{content}</span>
+            </section>
+        );
+    },
+    [KEYS.TOTAL_POSTS](value) {
+        return (
+            <section>
+                <span>{value}</span>
+            </section>
+        );
+    }
+};
+
+export const tileName = {
+    [KEYS.BUSIEST_DAY]: 'Busiest day',
+    [KEYS.HOT_STREAK]: 'Hot streak',
+    [KEYS.TOTAL_POSTS]: 'Total posts'
+};
+
 export class Tile extends Component<ITileProps, {}> {
     render() {
         const {recordKey: key, value} = this.props;
-        let content;
-        switch (key) {
-            case KEYS.BUSIEST_DAY:
-                content = value.count;
-                break;
 
-            case KEYS.HOT_STREAK:
-                const end = moment(value.end).utc();
-                if (moment().diff(end, 'days') > 0) {
-                    content = 0;
-                } else {
-                    content = moment(value.end).diff(moment(value.start), 'days');
-                }
-
-                break;
-
-            default:
-                content = value;
-                break;
-        }
-
-        return (<section>
-            <h4>{ key }</h4>
-            { content }
-        </section>);
+        return (
+            <section>
+                <header>
+                    <h4>{ tileName[key] }</h4>
+                </header>
+                { tileContentTemplate[key](value) }
+            </section>
+        );
     }
 }
